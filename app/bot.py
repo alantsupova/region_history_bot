80e3795e6f51fb095b95f96de16a00666db619f7
aleksandra <aleksaantsupova@yandex.ru>
2024-02-06 10:39:13 +0300

    fix closest places

--- app/bot.py
+++ app/bot.py
@@ -61,7 +61,7 @@ async def location(message: types.Message, state: FSMContext):
             lon = data.get("lon", "Unknown")
             if int(data.get("action", "Unknown")) == 4:
                 # places = requests.get(f'{conf["APP"]}/api/closest-places/?coordinate_x=60.000442&coordinate_y=30.329375&amount=2').json()
-                places = requests.get(f'http://127.0.0.1:8000/api/closest-places/?coordinate_x={lat}&coordinate_y={lon}&amount={n}').json()
+                places = requests.get(f'{conf["APP"]}/api/closest-places/?coordinate_x={lat}&coordinate_y={lon}&amount={n}').json()
                 for place in places:
                     await message.answer_location(latitude=place['coordinate_x'],
                                                   longitude=place['coordinate_y'])
